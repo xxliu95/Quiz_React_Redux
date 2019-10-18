8248f26722cacfe22b8a85e92bc273ce96c19584
Xinxin Liu <xxliu95@gmail.com>
2019-10-18 17:15:27 +0200

    estilo pagina home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default class Footer extends React.Component {
     render() {
         return(
-            <h5>Xinxin Liu</h5>
+            <div>
+                <h5>Xinxin Liu</h5>
+            </div>
         )
     }
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import {Link} from "react-router-dom";
-import {Button} from "@material-ui/core/es/index";
+import {Button, Paper, Typography} from "@material-ui/core/es/index";
 
 export default class Home extends React.Component {
     render() {
         return(
             <div className="App">
-                <Header/>
-                <div style={{padding: 20}}>
-                    <Link to="/play">
-                        <Button variant="contained" color="#abb1cf">
-                            PLAY NOW
-                        </Button>
-                    </Link>
+                <div style={{padding: 40}}>
+                    <Paper>
+                        <img src="reactredux.jpeg" style={{width: 350 }} alt=""/>
+                        <Typography variant="h1" component="h2">
+                            QUIZ GAME
+                        </Typography>
+                        <div style={{padding: 20}}>
+                            <Link to="/play" style={{ textDecoration: 'none' }}>
+                                <Button style={{backgroundColor: "#abb1cf"}}>
+                                    <Typography style={{padding:5, color: "white"}} variant="h5" component="h5">
+                                        PLAY NOW
+                                    </Typography>
+                                </Button>
+                            </Link>
+                        </div>
+                    </Paper>
                 </div>
                 <Footer/>
             </div>
